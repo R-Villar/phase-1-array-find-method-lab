9212d469c2db1596abd7a81510796516a4ac182f
Remberto Villar <rembertovillar@gmail.com>
2022-07-03 10:27:19 -0400

    commit tree f57dd85db488f88da3ca85e28099425872c8a35e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W")
+    console.log(result)
+    return result ? result.year : undefined
+}
